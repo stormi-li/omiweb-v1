d74eea096dcdd68da041fa661ca59a6e89bd83c9
Your Name <your.email@example.com>
2024-11-20 23:46:13 +0800

    修改了proxy

--- omiweb_Const.go
+++ omiweb_Const.go
@@ -2,12 +2,10 @@ package omiweb
 
 import (
 	"embed"
-	"time"
 )
 
 const target_path = "static"
 const index_path = "/index.html"
-const router_refresh_interval = 2 * time.Second
 
 //go:embed TemplateSource/*
 var templateSource embed.FS
