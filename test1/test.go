5301ea5e7e5e2d8f48b2c19373a1b2fde118af22
Your Name <your.email@example.com>
2024-11-26 01:24:18 +0800

    refine

--- test1/test.go
+++ test1/test.go
@@ -9,8 +9,8 @@ var redisAddr = "118.25.196.166:3934"
 var password = "12982397StrongPassw0rd"
 
 func main() {
-	omiweb := omiweb.NewClient(&redis.Options{Addr: redisAddr, Password: password})
-	omiweb.GenerateTemplate()
-	ws := omiweb.NewOmiWeb("test8084", "118.25.196.166:8084")
+	c := omiweb.NewClient(&redis.Options{Addr: redisAddr, Password: password})
+	c.GenerateTemplate()
+	ws := c.NewOmiWeb("test8084", "118.25.196.166:8084")
 	ws.Start(1)
 }
